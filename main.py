00d175180c30fdd7131117d8b5e8dc377fd0c959
astelmach01 <astelmach20@gmail.com>
2023-05-21 15:51:39 -0400

    fixed import

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from website import create_app, settings
+from website import create_app
+from website.settings import settings
 
 app = create_app()
 
 
 if __name__ == "__main__":
     debug = settings.ENVIRONMENT == "development"
-    app.run(debug=debug)
+    app.run(debug=False)
