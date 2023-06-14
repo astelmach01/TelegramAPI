a3a790052d508ad2176419ac894562d18e180d0a
astelmach01 <astelmach20@gmail.com>
2023-06-14 12:29:01 -0400

    added remove session files

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from hypercorn.config import Config
 import hypercorn.asyncio
 
 from website import create_app
+from website.util import remove_session_files
 
 app = create_app()
 
-
 async def start():
+    
+    remove_session_files()
+    
     config = Config()
     port = os.getenv("PORT", 8080)
     config.bind = [f"0.0.0.0:{port}"]
