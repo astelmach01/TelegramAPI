26b2e98e1fd6fbb22d175851da71481265344984
astelmach01 <astelmach20@gmail.com>
2023-06-20 22:18:39 -0400

    fixed sql query

--- website/queries/insert_telegram_credentials.sql
+++ website/queries/insert_telegram_credentials.sql
@@ -1,16 +1,19 @@
-INSERT INTO
-    `telegram_credentials` (
-        `phone_number`,
-        `telegram_api_id`,
-        `telegram_api_hash`,
-        `pipedrive_client_id`,
-        `pipedrive_client_secret`
-    )
-VALUES
-    (
-        '{{phone_number}}',
-        '{{telegram_api_id}}',
-        '{{telegram_api_hash}}',
-        '{{pipedrive_client_id}}',
-        '{{pipedrive_client_secret}}'
-    );
+INSERT INTO `telegram_credentials` (
+    `phone_number`,
+    `telegram_api_id`,
+    `telegram_api_hash`,
+    `pipedrive_client_id`,
+    `pipedrive_client_secret`
+)
+VALUES (
+    '{{phone_number}}',
+    '{{telegram_api_id}}',
+    '{{telegram_api_hash}}',
+    '{{pipedrive_client_id}}',
+    '{{pipedrive_client_secret}}'
+)
+ON DUPLICATE KEY UPDATE
+    `telegram_api_id` = VALUES(`telegram_api_id`),
+    `telegram_api_hash` = VALUES(`telegram_api_hash`),
+    `pipedrive_client_id` = VALUES(`pipedrive_client_id`),
+    `pipedrive_client_secret` = VALUES(`pipedrive_client_secret`);
